069fcb71e551754635d765aa1c216b5829eb0ab9
Parizat Ismailova <parizat.ismailova.kk@gmail.com>
2021-09-21 12:28:18 -0500

    JS Let and Const

---
+++ script.js
@@ -0,0 +1,11 @@
+let userName;
+userName = "Peri";
+document.write("<h1>" + userName + "</h1>");
+
+let userEmail;
+userEmail = "parizat.ismailova.kk@gmail.com";
+document.write("<h2>" + userEmail + "</h2>");
+
+let userNickname = prompt("Your nickname: ");
+document.write("<h1>Hello, " + userNickname + "</h1>");
+
